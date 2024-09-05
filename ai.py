a49fc2cfe5ebf8bab7a06b141d76ac67a6a0d2bc
reneczka <46569696+reneczka@users.noreply.github.com>
2024-09-05 15:29:11 +0200

    move ai logic to new module

---
+++ ai.py
@@ -0,0 +1,14 @@
+def generate_question():
+    print("# # # # # # # # # # # # Generating the question...")
+    return "This is the question"
+
+#TODO
+def generate_answer_evaluation():
+    print("# # # # # # # # # # # #Evaluating the user's answer...")
+    return "This is answer evaluation"
+
+# TODO
+def generate_final_evaluation():
+    print("# # # # # # # # # # # #Generating final evaluation...")
+    return "This is the final evaluation"
+
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 from pydantic import BaseModel
+import ai
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -16,82 +17,57 @@ class ReqMessage(BaseModel):
     jobId: int
     userMessage: str | None = None
 
+
 class ResMessage(BaseModel):
     aiMessage: list[str]
 
 message_count = 0
+last_question = ""
+
 
 @app.post("/message/")
 def send_message(
     req_message: ReqMessage, db: Session = Depends(get_db)) -> ResMessage:
-    global message_count
+    global message_count, last_question
 
-    if not req_message.userMessage:
-        message_count = 1
-        job = db.query(
-            models.Job).filter(models.Job.id == req_message.jobId).first()
-        if not job:
-            raise HTTPException(status_code=400, detail="No job found")
-
-        welcome_message = f"Welcome to the interview for the position of {job.job_name}. I'll be asking you some questions related to the job. Let's begin!"
-
-        # prompt = f"Create a job interview question for the following job description: {job.job_description}. Do not ask about user's past projects, ask technical questions."
-        # response = client.chat.completions.create(
-        #     model="gpt-4o-mini",
-        #     messages=[{
-        #         "role":
-        #         "system",
-        #         "content":
-        #         "You are an AI assistant that generates job interview questions."
-        #     }, {
-        #         "role": "user",
-        #         "content": prompt
-        #     }])
-
-        # interview_question = response.choices[0].message.content.strip()
-
-        #TODO! delete mock ansert
-        ai_mock = "This is mock ai answer"
-
-        print("# # # # # # # # # # # #", f"Message Count = {message_count}")
-
-        return ResMessage(aiMessage=[welcome_message, ai_mock])
-
-    message_count += 1
-
-    if message_count == 10:
-        final_evaluation = "Based on your answers, you have demonstrated a strong understanding of the required skills. Good luck with your job search!"
-        bye_bye_message = "Thank you for participating in this interview. Bye bye!"
-        print("# # # # # # # # # # # #", f"Message Count = {message_count}")
-        message_count = 0
-        return ResMessage(aiMessage=[final_evaluation, bye_bye_message])
-
-    # Otherwise, continue the interview with the next question
     job = db.query(
         models.Job).filter(models.Job.id == req_message.jobId).first()
     if not job:
         raise HTTPException(status_code=400, detail="No job found")
 
-    # prompt = f"Create a job interview question for the following job description: {job.job_description}. Do not ask about user's past projects, ask technical questions."
-    # response = client.chat.completions.create(
-    #     model="gpt-4o-mini",
-    #     messages=[{
-    #         "role":
-    #         "system",
-    #         "content":
-    #         "You are an AI assistant that generates job interview questions."
-    #     }, {
-    #         "role": "user",
-    #         "content": prompt
-    #     }])
+    if not req_message.userMessage:
+        message_count = 1
+        welcome_message = f"Welcome to the interview for the position of {job.job_name}. I'll be asking you some questions related to the job. Let's begin!"
+        last_question = ai.generate_question()
 
-    # interview_question = response.choices[0].message.content.strip()
+        print("# # # # # # # # # # # #", f"Message Count = {message_count}")
 
-    #TODO! delete mock answer
-    ai_mock = "This is mock ai answer"
+        return ResMessage(aiMessage=[welcome_message, last_question])
+
+    message_count += 1
+
+    evaluation = ai.generate_answer_evaluation()
+
+    if 1 < message_count < 10:
+        next_question = ai.generate_question()
+        last_question = next_question
+
+        print("# # # # # # # # # # # #", f"Message Count = {message_count}")
+        return ResMessage(aiMessage=[evaluation, next_question])
+
+    if message_count == 10:
+        final_evaluation = ai.generate_final_evaluation()
+        bye_bye_message = "Thank you for participating in this interview. Best of luck in your job search!"
+
+        print("# # # # # ## # # # #", f"Message Count = {message_count}")
+        message_count = 0
+        last_question = ""
+        return ResMessage(
+            aiMessage=[evaluation, final_evaluation, bye_bye_message])
 
     print("# # # # # # # # # # # #", f"Message Count = {message_count}")
-    return ResMessage(aiMessage=[ai_mock])
+    return ResMessage(aiMessage=[("this is a mock ai message")])
+
 
 if __name__ == "__main__":
     import uvicorn
